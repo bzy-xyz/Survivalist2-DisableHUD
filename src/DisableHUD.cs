5a7dd3cde1c5f1b62dbdf2b826c5309cb30f8d2f
Ben Z Yuan <bzy.xyz@gmail.com>
2021-03-14 22:03:45 -0700

    update for v115
    Fixes #1.
    Fixes #4.
    
    Partially addresses #3 by accident. I have no current plans to improve
    on this. ¯\_(ツ)_/¯

--- src/DisableHUD.cs
+++ src/DisableHUD.cs
@@ -3,6 +3,8 @@
 public class Main
 {
     public static Harmony HarmonyInstance;
+    public static HudBehaviour theHudBehaviour;
+    public static float originalSidebarWidth;
 
     public static void Load()
     {
@@ -16,6 +18,15 @@ public class Main
     public static void Unload()
     {
         UnityEngine.Debug.Log("[DisableHUD] unloading!");
+        if (theHudBehaviour != null) {
+            theHudBehaviour.SidebarRectTransform.SetInsetAndSizeFromParentEdge(
+                UnityEngine.RectTransform.Edge.Right,
+                0,
+                originalSidebarWidth
+            );
+            theHudBehaviour.transform.Find("SidebarDropShadow").gameObject.SetActive(true);
+            theHudBehaviour.SidebarRectTransform.Find("NamesPanel").gameObject.SetActive(true);
+        }
         if (HarmonyInstance != null)
         {
             HarmonyInstance.UnpatchAll();
@@ -42,20 +53,22 @@ static class SidebarRemoverPostfix
 {
     static void Postfix(ref HudBehaviour __instance)
     {
-        __instance.transform.Find("Sidebar").gameObject.SetActive(false);
+        Main.theHudBehaviour = __instance;
+        Main.originalSidebarWidth = __instance.SidebarRectTransform.rect.width;
+
+        __instance.SidebarRectTransform.SetInsetAndSizeFromParentEdge(
+            UnityEngine.RectTransform.Edge.Right,
+            0,
+            0
+        );
         __instance.transform.Find("SidebarDropShadow").gameObject.SetActive(false);
-        __instance.MainPanelRectTransform.offsetMax = new UnityEngine.Vector2(1920, 1080);
+        __instance.SidebarRectTransform.Find("NamesPanel").gameObject.SetActive(false);
 
-        UnityEngine.RectTransform vignette_rt = __instance.transform.Find("MainView/Vignette").GetComponent<UnityEngine.RectTransform>();
-        vignette_rt.offsetMax = new UnityEngine.Vector2(960, 540);
-
-        __instance.UnityGameCamera.aspect = 16.0f / 9.0f;
-
-        UnityEngine.RectTransform[] all_rt = UnityEngine.Object.FindObjectsOfType<UnityEngine.RectTransform>();
-        foreach (UnityEngine.RectTransform _rt in all_rt) {
-            string name = Utils.GetGameObjectPath(_rt.gameObject);
-            UnityEngine.Debug.Log($"{name} anchorMax {_rt.anchorMax} anchorMin {_rt.anchorMin} offsetMax {_rt.offsetMax} offsetMin {_rt.anchorMax}");
-        }
+        // UnityEngine.RectTransform[] all_rt = UnityEngine.Object.FindObjectsOfType<UnityEngine.RectTransform>();
+        // foreach (UnityEngine.RectTransform _rt in all_rt) {
+        //     string name = Utils.GetGameObjectPath(_rt.gameObject);
+        //     UnityEngine.Debug.Log($"{name} anchorMax {_rt.anchorMax} anchorMin {_rt.anchorMin} offsetMax {_rt.offsetMax} offsetMin {_rt.anchorMax}");
+        // }
 
         GameImpl.Instance.OnScreenResized();
     }
